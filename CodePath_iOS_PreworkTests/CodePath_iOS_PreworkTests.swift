35a3192adcb0a46896cd914ffc6db8d08431eec0
Anh Nguyen <rmn1@williams.edu>
2021-01-05 00:47:17 +0700

    Initial Commit

---
+++ CodePath_iOS_PreworkTests/CodePath_iOS_PreworkTests.swift
@@ -0,0 +1,33 @@
+//
+//  CodePath_iOS_PreworkTests.swift
+//  CodePath_iOS_PreworkTests
+//
+//  Created by Anh Mai Kieu Nguyen on 1/5/21.
+//
+
+import XCTest
+@testable import CodePath_iOS_Prework
+
+class CodePath_iOS_PreworkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
